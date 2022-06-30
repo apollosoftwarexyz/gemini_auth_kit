d60fee9d174938035e9ca528574a67b2549aecff
MrJoshE <josheverett02@gmail.com>
2022-06-30 09:32:41 +0100

    [Minor] added optional handleLoginFailure function for login failures

--- lib/data/gemini_response_handler.dart
+++ lib/data/gemini_response_handler.dart
@@ -1,5 +1,12 @@
+import 'package:gemini_auth_kit/data/failure.dart';
 import 'package:gemini_auth_kit/entities/gemini_login_response.dart';
 
 abstract class GeminiResponseHandler {
+  /// When we get a successful login response from gemini this function
+  /// will be called.
   Future<void> handleLoginResponse(RawGeminiLoginResponse response);
+
+  /// When there is a failure picked up by the gemini data layer
+  /// this function is called (this function should be optional)
+  Future<void> handleLoginFailure(GeminiFailure failure) async {}
 }
